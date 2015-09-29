6fd4748f40512d08868cf42ee81ac1f6377b93c8
pgdswd <pgdswd@lab-foss-pc>
2015-09-29 09:51:59 +0200

    3rd commit...

---
+++ JavaApplication18/src/newpackage/Apple.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author pgdswd
+ */
+public class Apple {
+
+    public Apple() {
+    }
+
+    @Override
+    public String toString() {
+        return "Apple{" + '}';
+    }
+    
+}
